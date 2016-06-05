a432cbaf73ad8f514a3d4e78b612666cdc4b7a5a
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-06-05 14:32:55 +0200

    Añadido valor por defecto a errors, así como nuevo servicio de muestreo de toasts

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -1,10 +1,14 @@
+defaultTime = 2000;
+
 //Muestra un error con el mensaje pasado por parametro. No realiza traducción
-throwError = function(message) {
+throwError = function(message, time) {
+    time = time || defaultTime;
     $('.toast').remove();
-    Materialize.toast('<b>' + message + '</b>', 2000, "toast-error");
+    Materialize.toast('<b>' + message + '</b>', time, "toast-error");
 }
 
-//Muestra un error traducido a través de la key pasada por argumento
-throwErrorTranslated = function(i18nKey){
-    throwError(TAPi18n.__(i18nKey));
+//Muestra un error traducido a través de la key pasada por argumento.
+throwErrorTranslated = function(i18nKey, time){
+    time = time || defaultTime;
+    throwError(TAPi18n.__(i18nKey), time);
 }
