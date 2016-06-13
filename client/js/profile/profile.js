abec7cc7a3d673339f3118ce1dedb93dae1aeeda
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-06-13 22:38:18 +0200

    Añadido paquete de utilidad para representar numeros de una forma mas amena

--- client/js/profile/profile.js
+++ client/js/profile/profile.js
@@ -1,3 +1,9 @@
 Template.profile.onRendered(function () {
     console.log(Meteor.user());
 });
+
+Template.profile.helpers({
+    friends_pretty: function(){
+        return Prettify.compactInteger(this.friends.length);
+    }
+});
