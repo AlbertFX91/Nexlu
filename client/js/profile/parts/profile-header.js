863ebe2003865efcdbfbcf044af4141d8dd20e84
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-06-15 18:53:59 +0200

    Finalizado esqueleto de mi perfil

--- client/js/profile/parts/profile-header.js
+++ client/js/profile/parts/profile-header.js
@@ -1,7 +1,3 @@
-Template.profileHeader.onRendered(function () {
-    console.log(Meteor.user());
-});
-
 Template.profileHeader.helpers({
     friends_pretty: function(){
         return Prettify.compactInteger(this.friends.length);
