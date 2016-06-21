466e78cb5518b3744adba67654cc5c80e4cb1776
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-06-21 17:19:08 +0200

    Modificado funcion de conversión para uso de metodos no deprecated

--- client/js/images/example/img-preview.js
+++ client/js/images/example/img-preview.js
@@ -43,23 +43,15 @@ Template.img_preview.helpers({
 
 });
 
+
+//http://stackoverflow.com/questions/4998908/convert-data-uri-to-file-then-append-to-formdata
 function dataURItoFile(img) {
-    // convert base64/URLEncoded data component to raw binary data held in a string
     var dataURI = img.result;
-    var byteString;
-    if (dataURI.split(',')[0].indexOf('base64') >= 0)
-        byteString = atob(dataURI.split(',')[1]);
-    else
-        byteString = unescape(dataURI.split(',')[1]);
-
-    // separate out the mime component
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-    // write the bytes of the string to a typed array
-    var ia = new Uint8Array(byteString.length);
-    for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
+    var type = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    var binary = atob(dataURI.split(',')[1]);
+    var array = [];
+    for(var i = 0; i < binary.length; i++) {
+        array.push(binary.charCodeAt(i));
     }
-
-    return new File([ia], img.name, {type: mimeString});
+    return new File([new Uint8Array(array)], img.name, {type: type});
 }
