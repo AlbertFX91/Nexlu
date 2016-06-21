a4144ea6dcfea13d91230b9e7bb3e2a614582dce
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-06-21 17:39:10 +0200

    modificado source de la imagen para mostrar la almacenada en s3

--- client/js/images/example/img-preview.js
+++ client/js/images/example/img-preview.js
@@ -1,24 +1,11 @@
 Template.img_preview.events({
     "change input.file_bag": function(e){
         var file = $(e.target)[0].files[0];
-        console.log(file);
-        //var id = ImagesLocals.insert(file);
-
-        //console.log("ID: "+id);
-/*
-        $(".img-containers").append('<div class="col s1"><img src="#" alt=" " id="'+id+'" /></div>');
-
-        var reader = new FileReader();
-        reader.onload = function (e){
-            $('#'+id).attr('src', e.target.result);
-        };
-        reader.readAsDataURL(file);
- */     //readAsDataURL(ImagesLocals.findOne(id));
         var reader = new FileReader();
         reader.onload = function (e){
             var id = ImagesLocals.insert(_.extend({result: e.target.result},file));
-            console.log("ID FILE: "+id);
-            //$('#'+id).attr('src', e.target.result);
+            Toasts.throw("Img loaded in your browser!",5000);
+
         };
         reader.readAsDataURL(file);
     },
@@ -31,6 +18,7 @@ Template.img_preview.events({
             path:"users"
         },function(e,r){
             Toasts.throw("Image uploaded! click &nbsp;<a href='"+r.url+"' target='blank' />here</a>&nbsp;", 10000);
+            $("#"+img_id).attr("src", r.url);
             //console.log(r);
         });
     }
