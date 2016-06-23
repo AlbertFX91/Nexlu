aa889618c2c98e6ffcd12196af1984b4506a235f
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-06-23 18:12:38 +0200

    Subida de multiples imagenes al navegador

--- client/js/images/images-input-modal.js
+++ client/js/images/images-input-modal.js
@@ -1,24 +1,13 @@
 Template.images_input_modal.events({
     "click .close-modal-button": function(e){
         e.preventDefault();
-        console.log("HI!");
         $("#input-images-modal").closeModal();
     },
     "change #image-upload": function(e){
-        //Obtenemos el fichero del input
-        var file = $(e.target)[0].files[0];
-
-        //Declaramos el objeto FileReader que usaremos para convertir el fichero en una URL para poder previsualizarlo y almacenarlo en la collection
-        var reader = new FileReader();
-        reader.onload = function (e){
-            //Declaramos que una vez cargado un fichero, insertaremos en la collection local ImagesLocales los datos del fichero,
-            //así como los datos del fichero en una url, y un atributo auxiliar que nos indicará si se ha subido o no
-            var id = ImagesLocals.insert(_.extend({result: e.target.result, uploaded: false},file));
-            Toasts.throw("Img loaded in your browser!",5000);
-
-        };
-        //Aquí leemos el fichero y se ejecutará la función onload una vez cargado
-        reader.readAsDataURL(file);
+        //Obtenemos los ficheros seleccionados
+        var files = $(e.target)[0].files;
+        //Para cada fichero, almacenamos el fichero en el navegador
+        _.each(files, saveImgInBrowserByFile);
     }
 });
 
@@ -30,3 +19,18 @@ Template.images_input_modal.helpers({
         return ImagesLocals.find();
     }
 });
+
+
+function saveImgInBrowserByFile(file){
+    //Declaramos el objeto FileReader que usaremos para convertir el fichero en una URL para poder previsualizarlo y almacenarlo en la collection
+    var reader = new FileReader();
+    reader.onload = function (e){
+        //Declaramos que una vez cargado un fichero, insertaremos en la collection local ImagesLocales los datos del fichero,
+        //así como los datos del fichero en una url, y un atributo auxiliar que nos indicará si se ha subido o no
+        var id = ImagesLocals.insert(_.extend({result: e.target.result, uploaded: false},file));
+        Toasts.throw("Img loaded in your browser!",1000);
+
+    };
+    //Aquí leemos el fichero y se ejecutará la función onload una vez cargado
+    reader.readAsDataURL(file);
+}
