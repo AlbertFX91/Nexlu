90c47e87514275ce632e37199c1d23ee7c11bc7d
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-06-23 16:37:44 +0200

    Creado boton de despliegue de modal de input de imagenes

---
+++ client/js/images/images-input.js
@@ -0,0 +1,10 @@
+Template.images_input.onRendered(function(){
+   
+});
+
+Template.images_input.events({
+    "click #button-show-input-modal": function(e){
+        e.preventDefault();
+        $('#input-images-modal').openModal();
+    }
+});
