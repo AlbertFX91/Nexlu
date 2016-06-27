c0583c5e00d525c57cb41757b7db1d267786f590
FraVieMor <fraviemor@gmail.com>
2016-06-27 20:15:26 +0200

    Modificada query de las publicaciones mostradas en timeline

--- client/js/timeline.js
+++ client/js/timeline.js
@@ -1,6 +1,9 @@
 Template.timeline.helpers({
     publications: function () {
         var user_id = Meteor.userId();
-        return Publications.find({"owner.0.id": user_id},{sort: {createdAt: 1}});
+
+        //return Publications.find({"owner.0.id": user_id},{sort: {createdAt: 1}});
+        //return Publications.find({"owner.0.id": user_id});
+        return Publications.find();
     }
 });
