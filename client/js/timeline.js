f639118e62a174594970561555cf9e6c0d801bd7
FraVieMor <fraviemor@gmail.com>
2016-06-27 20:53:08 +0200

    Ordenadas las publicaciones del timeline

--- client/js/timeline.js
+++ client/js/timeline.js
@@ -2,8 +2,6 @@ Template.timeline.helpers({
     publications: function () {
         var user_id = Meteor.userId();
 
-        //return Publications.find({"owner.0.id": user_id},{sort: {createdAt: 1}});
-        //return Publications.find({"owner.0.id": user_id});
-        return Publications.find();
+        return Publications.find({},{sort: {createdAt: -1}});
     }
 });
