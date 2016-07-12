42a7a869dcd6df073490d89903e30a9adc004275
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-07-12 20:56:34 +0200

    Animacion de muestra de panel de chat y de cierre

--- client/js/chat/chat-menu.js
+++ client/js/chat/chat-menu.js
@@ -1,3 +1,9 @@
 Template.chat_menu.onRendered(function(){
     $("#chat-list-body").niceScroll();
 });
+
+Template.chat_menu.events({
+    "click #chat-list-close-button": function(){
+        $("#chat-list-container").removeClass("fadeInUp").addClass("fadeOutDown");
+    }
+});
