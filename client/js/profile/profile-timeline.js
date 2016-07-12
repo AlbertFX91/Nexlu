61b4596fd35a3a3dc377fd19273728ecb3b63ae5
FraVieMor <fraviemor@gmail.com>
2016-07-12 21:57:23 +0200

    agregadas mis pubs a mi perfil

---
+++ client/js/profile/profile-timeline.js
@@ -0,0 +1,6 @@
+Template.profileTimeline.helpers({
+    publications: function () {
+        var user_id = Meteor.userId();
+        return Publications.find({"owner.0.id": user_id},{sort: {createdAt: -1}});
+    }
+});
