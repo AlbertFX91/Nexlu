4b02d4176d441682cb91a22f147b74ec29836d37
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-07-19 23:44:28 +0200

    Aviso de nuevo mensaje en el chat al recibirlo

--- client/js/chat/chat-menu-user-entry.js
+++ client/js/chat/chat-menu-user-entry.js
@@ -10,6 +10,7 @@ Template.chat_menu_user_entry.events({
             }else{
                 Session.set("ChatRoom.id", id);
             }
+            $("#chat-menu-user-"+follower_id).children(".new-message-container").removeClass("new");
         })
     }
 });
--- client/js/chat/chat-menu.js
+++ client/js/chat/chat-menu.js
@@ -2,9 +2,26 @@ Template.chat_menu.onRendered(function(){
     Session.set("ChatRoom.id", null);
     $("#chat-list-body").niceScroll();
     Meteor.subscribe("user.each.chat");
-    var handle = Meteor.users.find({_id: {$ne: Meteor.userId()}}).observeChanges({
+
+    Meteor.subscribe("chatroom.mine");
+    var handle = ChatRooms.find({"players.id": Meteor.userId()}).observeChanges({
         changed: function(id, fields){
-            console.log("CHANGED ["+id+"]: "+fields);
+            //Recupero todos los mensajes del chatroom modificado
+            var messages = _.sortBy(fields.messages, function(m){return m.createdAt});
+            //Recupero el ultimo mensaje para comprobar que el mensaje no lo he enviado yo, sino que lo he recibido.
+            var lastMessage = messages[messages.length-1];
+            if(lastMessage.player != Meteor.userId()){
+                var chatroom_id = Session.get("ChatRoom.id");
+                var chatOpen = chatroom_id !=undefined && chatroom_id == id;
+                
+                if(!chatOpen){
+                    var player_id = lastMessage.player;
+                    $("#chat-menu-user-"+player_id).children(".new-message-container").addClass("new");
+                }
+            }
+            /*console.log("CHANGED ["+id+"]");
+            console.log(fields);
+            console.log("Last message: "+messages[messages.length-1].message);*/
         }
     })
 });
