1852f5b8b61792873b8802c28a4767cc95bf19f6
FraVieMor <fraviemor@gmail.com>
2016-07-23 21:27:07 +0200

    resuelto problema al crear comentario

--- client/js/timeline/newComment.js
+++ client/js/timeline/newComment.js
@@ -1,17 +1,17 @@
 Template.newComment.events({
     'submit form': function(e) {
         e.preventDefault();
-        var description = $("textarea#newComment").val();
+        var description = e.target.newComment.value;
         var userId = Meteor.userId();
         var publicationId = this._id;
         var valido = true;
         if (description.trim() == ""){
             var texto = TAPi18n.__("error.post-notBlank");
-            document.getElementById('comment-error').innerHTML = texto;
+            $(e.target).children('.new-comment-action').children('#commentError').text(texto);
             valido = false;
         } else if (description.length > 2000) {
             var texto = TAPi18n.__("error.post-maxlength");
-            document.getElementById('comment-error').innerHTML = texto;
+            $(e.target).children('.new-comment-action').children('#commentError').text(texto);
             valido = false;
         }
         var comment = {
@@ -24,14 +24,14 @@ Template.newComment.events({
         if (valido) {
             Meteor.call('postComment', publicationId, comment, function(err, response) {
                 if (!err){
-                    var textarea = $("textarea#newComment").val("");
-                    $("#newPublication").trigger('autoresize');
+                    e.target.newComment.value = "";
+                    $("#newComment").trigger('autoresize');
                 }
             });
         }
     },
     'click #newComment': function(e) {
         e.preventDefault();
-        $(e.target).parent().parent().next().children('#comment-error').text("");
+        $(e.target).parent().parent().next().children('#commentError').text("");
     }
 });
