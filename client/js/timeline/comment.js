a784e63db048ae50e977678d5c652b67588bad26
FraVieMor <fraviemor@gmail.com>
2016-07-26 06:44:37 +0200

    editar/eliminar comentario (sin funcionalidad)

--- client/js/timeline/comment.js
+++ client/js/timeline/comment.js
@@ -11,10 +11,83 @@ Template.comment.helpers({
     },
     iDislike: function() {
         return _.contains(this.playersDislike, Meteor.userId().trim());
+    },
+    isMine: function() {
+        if (this.player.trim() === Meteor.userId().trim())
+            return true;
+        return false;
+    },
+    settingsTextareaEditComment: function () {
+        return {
+            position: top,
+            limit: 5,
+            rules: [
+                {
+                    token: '@',
+                    collection: Meteor.users,
+                    field: 'username',
+                    options: '',
+                    template: Template.userPill,
+                    noMatchTemplate: Template.notMatch
+                }
+            ]
+        }
     }
 });
 
 Template.comment.events({
+    'click #edit-comment': function () {
+        $('#edit-comment-modal').openModal({complete:function(){
+            document.getElementById('edit-comment-error').innerHTML = "";
+        }})
+        var textarea = document.getElementById('editComment');
+        textarea.value = this.description;
+        $("#editComment").trigger('autoresize');
+        $("#edit-comment-label").addClass("active");
+    },
+    'click #remove-comment': function () {
+        $('#remove-comment-modal').openModal();
+    }
+    ,
+    'submit .edit-comment': function(e) {
+        e.preventDefault();
+        var description = document.getElementById('editComment').value;
+        var commentId = this.id;
+        var valido = true;
+        if (description.trim() == ""){
+            var texto = TAPi18n.__("error.comment-notBlank");
+            document.getElementById('edit-comment-error').innerHTML = texto;
+            $("#edit-comment-label").removeClass("active");
+            valido = false;
+        } else if (description.length > 2000) {
+            var texto = TAPi18n.__("error.comment-maxlength");
+            document.getElementById('edit-comment-error').innerHTML = texto;
+            $("#edit-comment-label").hide();
+            valido = false;
+        }
+        /**if (valido) {
+            Meteor.call('editComment', commentId, description, function(err, response){
+                if (!err){
+                    $('#edit-comment-modal').closeModal();
+                }
+            });
+        }**/
+    },
+    'click #editComment': function(e) {
+        e.preventDefault();
+        document.getElementById('edit-comment-error').innerHTML = "";
+        $("#edit-comment-label").show();
+    },
+    'submit .remove-comment': function(e) {
+        e.preventDefault();
+        var commentId = this.id;
+        /**Meteor.call('removeComment', commentId, function(err, response){
+            if (!err){
+                $('#remove-comment-modal').closeModal();
+                $('.lean-overlay').remove();
+            }
+        });**/
+    },
     'click #like': function (e) {
         e.preventDefault();
         var commentId = this.id;
--- client/js/timeline/newComment.js
+++ client/js/timeline/newComment.js
@@ -1,3 +1,22 @@
+Template.newComment.helpers({
+    settingsTextareaNewComment: function () {
+        return {
+            position: top,
+            limit: 5,
+            rules: [
+                {
+                    token: '@',
+                    collection: Meteor.users,
+                    field: 'username',
+                    options: '',
+                    template: Template.userPill,
+                    noMatchTemplate: Template.notMatch
+                }
+            ]
+        }
+    }
+});
+
 Template.newComment.events({
     'submit form': function(e) {
         e.preventDefault();
@@ -6,11 +25,11 @@ Template.newComment.events({
         var publicationId = this._id;
         var valido = true;
         if (description.trim() == ""){
-            var texto = TAPi18n.__("error.post-notBlank");
+            var texto = TAPi18n.__("error.comment-notBlank");
             $(e.target).children('.new-comment-action').children('#commentError').text(texto);
             valido = false;
         } else if (description.length > 2000) {
-            var texto = TAPi18n.__("error.post-maxlength");
+            var texto = TAPi18n.__("error.comment-maxlength");
             $(e.target).children('.new-comment-action').children('#commentError').text(texto);
             valido = false;
         }
