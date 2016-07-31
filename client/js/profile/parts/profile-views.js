5384363718d276675aefb8b55793c0169e2780fa
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-07-31 02:49:27 +0200

    Corregido error

--- client/js/profile/parts/profile-views.js
+++ client/js/profile/parts/profile-views.js
@@ -1,11 +1,14 @@
 Template.profileViewsUser.helpers({
     canSee: function(){
+        var user = Meteor.user();
+        if(user._id == this.user._id){
+            return true;
+        }
         var isPrivate = this.user.private_profile;
         if(!isPrivate){
             return true;
         }
         var isFollowed = false;
-        var user = Meteor.user();
         if(user){
             isFollowed = _.contains(user.followed, this.user._id);
         }
