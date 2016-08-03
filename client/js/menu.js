d3d1e2eda9edee5d305c84dc3a9b661a611ee227
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-08-03 11:24:32 +0200

    Creado despliegue de ventana

--- client/js/menu.js
+++ client/js/menu.js
@@ -4,13 +4,17 @@ Template.menu.events({
     },
     'click #logout': function(event){
   		$('#user-menu').trigger('mouseleave');
-  			Meteor.logout(function(err) {
-                if (!err) {
-                    Router.go("/");
-                    Materialize.toast('<b>' + TAPi18n.__("logout_success") + '</b>', 2700);
-                }
-            });
-		}
+        Meteor.logout(function(err) {
+            if (!err) {
+                Router.go("/");
+                Materialize.toast('<b>' + TAPi18n.__("logout_success") + '</b>', 2700);
+            }
+        });
+    }
+});
+
+Template.menuLogged.onRendered(function(e) {
+    $("#notif-container").niceScroll();
 });
 
 Template.menuLogged.helpers({
@@ -27,5 +31,18 @@ Template.menuLogged.helpers({
     numNotifications: function(){
         var length = _.filter(Meteor.user().notifications, function(n){return !n.watched}).length;
         return length > 50? "+50":length
+    }
+});
+
+Template.menuLogged.events({
+    'click #badge-button': function(e){
+        e.preventDefault();
+        if($("#notif-container").css("display") == "none"){
+            $("#div1").css("display", "block");
+            $("#notif-container").css("display", "block");
+        }else{
+            $("#div1").css("display", "none");
+            $("#notif-container").css("display", "none");
+        }
     }
 });
