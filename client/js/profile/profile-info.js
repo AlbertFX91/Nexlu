2d91c41dc45718b5c36e02eb87d40e645e7050ce
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-08-03 17:27:34 +0200

    corregido error en helper

--- client/js/profile/profile-info.js
+++ client/js/profile/profile-info.js
@@ -4,8 +4,11 @@ Template.profileInfoUser.helpers({
         if(!isPrivate){
             return true;
         }
-        var isFollowed = false;
         var user = Meteor.user();
+        if(user && user._id == this.user._id){
+            return true;
+        }
+        var isFollowed = false;
         if(user){
             isFollowed = _.contains(user.followed, this.user._id);
         }
