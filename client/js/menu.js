f025de8b540510a1ffeb8fb02998faacba3b1dbb
Cristian Poley <cripolgon@gmail.com>
2016-08-04 00:23:20 +0200

    Añadido logo en menu con transicion en NE y LU

--- client/js/menu.js
+++ client/js/menu.js
@@ -10,5 +10,13 @@ Template.menu.events({
                     Materialize.toast('<b>' + TAPi18n.__("logout_success") + '</b>', 2700);
                 }
             });
-		}
+    },
+    'mouseenter .boton-logo-nav':function(){
+        $('#text-logo-ne').css('color', 'white');
+        $('#text-logo-lu').css('color', 'white');
+    },
+    'mouseleave .boton-logo-nav':function(){
+        $('#text-logo-lu').css('color', 'transparent');
+        $('#text-logo-ne').css('color', 'transparent');
+    }
 });
